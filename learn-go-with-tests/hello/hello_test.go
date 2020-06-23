5425bbcaeec92c6ba96b923dee9520fda32563f7
Victor Menegusso <victor.menegusso@contabilizei.com.br>
2020-06-23 09:54:25 -0300

    add French

--- learn-go-with-tests/hello/hello_test.go
+++ learn-go-with-tests/hello/hello_test.go
@@ -4,21 +4,34 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	assertCorrectMessage := func(t *testing.T, got, want string) {
-		//t.Helper()
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'World'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jack", "French")
+		want := "Bonjour, Jack"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
